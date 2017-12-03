a944bb60d0d82d9f76034cf6a0f0a4efce596a25
Shantanu Odak <so2362njit.edu>
2017-12-02 21:54:01 -0500

    added css on the home page

---
+++ Login1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: shantanuodak
+ * Date: 12/2/17
+ * Time: 9:46 PM
+ */
---
+++ register1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shantanuodak
+ * Date: 12/2/17
+ * Time: 9:47 PM
+ */
